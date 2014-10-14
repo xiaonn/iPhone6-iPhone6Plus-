cc7faf130146450fd00ef37bdcc86fd6d6460a93
xiaonn <lsyyz9687@163.com>
2014-10-14 10:07:26 +0800

    Initial Commit

---
+++ UILayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILayout
+//
+//  Created by 李帅 on 14-10-14.
+//  Copyright (c) 2014年 李帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UILayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UILayout
+//
+//  Created by 李帅 on 14-10-14.
+//  Copyright (c) 2014年 李帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
